dc299e834db0a78af941bbfd86203af41734d61f
Miquel Campos <miquel.campos@gmail.com>
2016-11-14 22:19:33 +0900

    Adding maya 2017 scons .bat

--- buildAll.bat
+++ buildAll.bat
@@ -1,6 +1,8 @@
 @echo off
 
+
 scons with-maya=2014
 scons with-maya=2015
-scons with-maya=2016 with-mayadevkit="C:/Program Files/Autodesk/Maya2016/devkitBase"
+scons with-maya=2016
+scons with-maya=2017
 
