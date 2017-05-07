82dd48d06dfe3b7e108ef801d39d5233866e6828
Miquel Campos <miquel.campos@gmail.com>
2017-05-07 22:22:10 +0900

    Rigbits first commit

--- scripts/mGear_menu.py
+++ scripts/mGear_menu.py
@@ -28,7 +28,6 @@ from functools import partial
 import pymel.core as pm
 
 #mGear Tools
-import mGear_riggingTools
 import mGear_postSpring
 import mGear_proxySlicer
 import mGear_rope
@@ -42,6 +41,9 @@ import mGear_mocapTools
 import mgear
 import mgear.maya.synoptic as syn
 import mgear.maya.skin as skin
+
+#import rigbits
+import mgear.maya.rigbits as rigbits
 
 
 def CreateMenu():
@@ -68,55 +70,55 @@ def CreateMenu():
 
     ## Rigging Tools
     riggingM = pm.menuItem(parent='mGear', subMenu=True, tearOff=True, label='Rigging')
-    pm.menuItem(label="Add NPO", command=partial(mGear_riggingTools.addNPO))
+    pm.menuItem(label="Add NPO", command=partial(rigbits.addNPO))
     pm.menuItem(subMenu=True, tearOff=True, label='Gimmick Joints')
-    pm.menuItem(label="Add Joint", command=partial(mGear_riggingTools.addJnt, False, False))
+    pm.menuItem(label="Add Joint", command=partial(rigbits.addJnt, False, False))
     pm.menuItem( divider=True )
-    pm.menuItem(label="Add Blended Joint", command=partial(mGear_riggingTools.addBlendedJoint, False, False))
-    pm.menuItem(label="Add Support Joint", command=partial(mGear_riggingTools.addSupportJoint, False, False))
+    pm.menuItem(label="Add Blended Joint", command=partial(rigbits.addBlendedJoint, False, False))
+    pm.menuItem(label="Add Support Joint", command=partial(rigbits.addSupportJoint, False, False))
     pm.setParent(riggingM, menu=True)
     pm.menuItem( divider=True )
-    pm.menuItem(label="Replace Shape", command=partial(mGear_riggingTools.replaceShape))
+    pm.menuItem(label="Replace Shape", command=partial(rigbits.replaceShape))
     pm.menuItem( divider=True )
-    pm.menuItem(label="Match All Transform", command=partial(mGear_riggingTools.matchWorldXform))
-    pm.menuItem(label="Match Pos with BBox", command=partial(mGear_riggingTools.matchPosfromBBox))
-    pm.menuItem(label="Align Ref Axis", command=partial(mGear_riggingTools.alignToPointsLoop, None, None, None))
+    pm.menuItem(label="Match All Transform", command=partial(rigbits.matchWorldXform))
+    pm.menuItem(label="Match Pos with BBox", command=partial(rigbits.matchPosfromBBox))
+    pm.menuItem(label="Align Ref Axis", command=partial(rigbits.alignToPointsLoop, None, None, None))
 
     pm.setParent(riggingM, menu=True)
     pm.menuItem( divider=True )
     pm.menuItem(subMenu=True, tearOff=True, label='CTL as Parent')
-    pm.menuItem(label="Square", command=partial(mGear_riggingTools.createCTL, "square"))
-    pm.menuItem(label="Circle", command=partial(mGear_riggingTools.createCTL, "circle"))
-    pm.menuItem(label="Cube", command=partial(mGear_riggingTools.createCTL, "cube"))
-    pm.menuItem(label="Diamond", command=partial(mGear_riggingTools.createCTL, "diamond"))
-    pm.menuItem(label="Sphere", command=partial(mGear_riggingTools.createCTL, "sphere"))
-    pm.menuItem(label="Cross", command=partial(mGear_riggingTools.createCTL, "cross"))
-    pm.menuItem(label="Cross Arrow", command=partial(mGear_riggingTools.createCTL, "crossarrow"))
-    pm.menuItem(label="Pyramid", command=partial(mGear_riggingTools.createCTL, "pyramid"))
-    pm.menuItem(label="Cube With Peak", command=partial(mGear_riggingTools.createCTL, "cubewithpeak"))
+    pm.menuItem(label="Square", command=partial(rigbits.createCTL, "square"))
+    pm.menuItem(label="Circle", command=partial(rigbits.createCTL, "circle"))
+    pm.menuItem(label="Cube", command=partial(rigbits.createCTL, "cube"))
+    pm.menuItem(label="Diamond", command=partial(rigbits.createCTL, "diamond"))
+    pm.menuItem(label="Sphere", command=partial(rigbits.createCTL, "sphere"))
+    pm.menuItem(label="Cross", command=partial(rigbits.createCTL, "cross"))
+    pm.menuItem(label="Cross Arrow", command=partial(rigbits.createCTL, "crossarrow"))
+    pm.menuItem(label="Pyramid", command=partial(rigbits.createCTL, "pyramid"))
+    pm.menuItem(label="Cube With Peak", command=partial(rigbits.createCTL, "cubewithpeak"))
     pm.setParent(riggingM, menu=True)
     pm.menuItem(subMenu=True, tearOff=True, label='CTL as Child')
-    pm.menuItem(label="Square", command=partial(mGear_riggingTools.createCTL, "square", True))
-    pm.menuItem(label="Circle", command=partial(mGear_riggingTools.createCTL, "circle", True))
-    pm.menuItem(label="Cube", command=partial(mGear_riggingTools.createCTL, "cube", True))
-    pm.menuItem(label="Diamond", command=partial(mGear_riggingTools.createCTL, "diamond", True))
-    pm.menuItem(label="Sphere", command=partial(mGear_riggingTools.createCTL, "sphere", True))
-    pm.menuItem(label="Cross", command=partial(mGear_riggingTools.createCTL, "cross", True))
-    pm.menuItem(label="Cross Arrow", command=partial(mGear_riggingTools.createCTL, "crossarrow", True))
-    pm.menuItem(label="Pyramid", command=partial(mGear_riggingTools.createCTL, "pyramid", True))
-    pm.menuItem(label="Cube With Peak", command=partial(mGear_riggingTools.createCTL, "cubewithpeak", True))
+    pm.menuItem(label="Square", command=partial(rigbits.createCTL, "square", True))
+    pm.menuItem(label="Circle", command=partial(rigbits.createCTL, "circle", True))
+    pm.menuItem(label="Cube", command=partial(rigbits.createCTL, "cube", True))
+    pm.menuItem(label="Diamond", command=partial(rigbits.createCTL, "diamond", True))
+    pm.menuItem(label="Sphere", command=partial(rigbits.createCTL, "sphere", True))
+    pm.menuItem(label="Cross", command=partial(rigbits.createCTL, "cross", True))
+    pm.menuItem(label="Cross Arrow", command=partial(rigbits.createCTL, "crossarrow", True))
+    pm.menuItem(label="Pyramid", command=partial(rigbits.createCTL, "pyramid", True))
+    pm.menuItem(label="Cube With Peak", command=partial(rigbits.createCTL, "cubewithpeak", True))
     pm.setParent(riggingM, menu=True)
     pm.menuItem( divider=True )
-    pm.menuItem(label="Duplicate symmetrical", command=partial(mGear_riggingTools.duplicateSym))
+    pm.menuItem(label="Duplicate symmetrical", command=partial(rigbits.duplicateSym))
     pm.menuItem( divider=True )
-    pm.menuItem(label="Space Jumper", command=partial(mGear_riggingTools.spaceJump))
-    pm.menuItem(label="Interpolated Transform", command=partial(mGear_riggingTools.createInterpolateTransform))
+    pm.menuItem(label="Space Jumper", command=partial(rigbits.spaceJump))
+    pm.menuItem(label="Interpolated Transform", command=partial(rigbits.createInterpolateTransform))
 
     pm.menuItem(subMenu=True, tearOff=True, label='Connect local SRT')
-    pm.menuItem(label="connect SRT", command=partial(mGear_riggingTools.connectLocalTransfrom, 1,1,1))
-    pm.menuItem(label="connect T", command=partial(mGear_riggingTools.connectLocalTransfrom, 0,0,1))
-    pm.menuItem(label="connect R", command=partial(mGear_riggingTools.connectLocalTransfrom, 0,1,0))
-    pm.menuItem(label="connect S", command=partial(mGear_riggingTools.connectLocalTransfrom, 1,0,0))
+    pm.menuItem(label="connect SRT", command=partial(rigbits.connectLocalTransfrom, 1,1,1))
+    pm.menuItem(label="connect T", command=partial(rigbits.connectLocalTransfrom, 0,0,1))
+    pm.menuItem(label="connect R", command=partial(rigbits.connectLocalTransfrom, 0,1,0))
+    pm.menuItem(label="connect S", command=partial(rigbits.connectLocalTransfrom, 1,0,0))
     pm.setParent(riggingM, menu=True)
     pm.menuItem( divider=True )
     pm.menuItem(label="Spring", command=partial(mGear_postSpring.spring_UI))
---
+++ scripts/mgear/maya/rigbits/ghost.py
@@ -0,0 +1,75 @@
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# Author:     Jeremie Passerin      geerem@hotmail.com  www.jeremiepasserin.com
+# Author:     Miquel Campos         hello@miquel-campos.com  www.miquel-campos.com
+# Date:       2016 / 10 / 10
+
+import pymel.core as pm
+
+import mgear.maya.rigbits as rigbits
+
+
+def createGhostCtl(ctl, parent=None):
+    if  isinstance(ctl, basestring):
+        ctl = pm.PyNode(ctl)
+    if parent:
+        if  isinstance(parent, basestring):
+            parent = pm.PyNode(parent)
+
+    oName = ctl.name()
+    pm.rename(ctl, oName + "_ghost")
+    newCtl =   pm.duplicate(ctl, po=True)[0]
+    pm.rename(newCtl, oName)
+    source2 = pm.duplicate(ctl)[0]
+    for shape in  source2.getShapes():
+        pm.parent(shape, newCtl, r=True, s=True)
+        pm.rename( shape, newCtl.name() + "Shape" )
+        pm.parent(shape, newCtl, r=True, s=True)
+    pm.delete(source2)
+    if parent:
+        pm.parent(newCtl, parent)
+        oTra = pm.createNode("transform", n= newCtl.name() + "_npo", p=parent, ss=True)
+        oTra.setTransformation(newCtl.getMatrix())
+        pm.parent(newCtl, oTra)
+   
+    rigbits.connectLocalTransforms(newCtl, ctl)
+    
+
+
+    return newCtl
+    
+    
+def createDoritoGhostCtl(ctl, parent=None):
+    if  isinstance(ctl, basestring):
+        ctl = pm.PyNode(ctl)
+    if parent:
+        if  isinstance(parent, basestring):
+            parent = pm.PyNode(parent)
+    doritoCtl = createGhostCtl(ctl, parent)
+    doritoParent = doritoCtl.getParent()
+    ghostBaseParent = ctl.getParent(2)
+    oTra = pm.createNode("transform", n= ghostBaseParent.name() + "_npo", p=ghostBaseParent.getParent(), ss=True)
+    oTra.setTransformation(ghostBaseParent.getMatrix())
+    pm.parent(ghostBaseParent, oTra)
+    oTra = pm.createNode("transform", n= doritoParent.name() + "_npo", p=doritoParent.getParent(), ss=True)
+    oTra.setTransformation(doritoParent.getMatrix())
+    pm.parent(doritoParent, oTra)
+    
+    rigbits.connectLocalTransforms(ghostBaseParent, doritoParent)
