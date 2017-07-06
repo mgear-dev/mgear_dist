a785092f595c87ff67febfc074bf84544316f826
miquelcampos <miquel.campos@gmail.com>
2017-07-06 16:41:52 +0900

    mGear: Simple autorig system for props and set dressin. This rigging system follow the structure and naming conventions from Shifer.

--- scripts/mGear_menu.py
+++ scripts/mGear_menu.py
@@ -41,6 +41,9 @@ import mgear
 import mgear.maya.synoptic as syn
 import mgear.maya.skin as skin
 
+# simple rig
+import mgear.maya.simpleRig as srig
+
 #import rigbits
 import mgear.maya.rigbits as rigbits
 import mgear.maya.rigbits.postSpring as postSpring
@@ -78,6 +81,19 @@ def CreateMenu():
     pm.menuItem(label="Update Guide", command=partial(mGear_guidesTemplates.updateGuide))
 
 
+    pm.setParent(mGearM, menu=True)
+    pm.menuItem( divider=True )
+
+    #simple rig
+
+    pm.menuItem(parent='mGear', subMenu=True, tearOff=True, label='Simple Rig')
+    pm.menuItem(label="Simple Rig Generate", command=partial(srig.simpleRig, "rig", False))
+    pm.menuItem( divider=True )
+    pm.menuItem(label="Create Root", command=partial(srig.createRoot))
+    pm.menuItem( divider=True )
+    pm.menuItem(label="Set User Pivot", command=partial(srig.setUserRigPivot))
+    pm.menuItem(label="Add To User Pivot", command=partial(srig.addToUserPivot))
+    pm.menuItem(label="Select Objects In User Pivot", command=partial(srig.selectObjectInUserRootPivot))
     pm.setParent(mGearM, menu=True)
     pm.menuItem( divider=True )
 
