0584eeb79fb465834e968fe73b292577a3ed3521
miquelcampos <miquel.campos@gmail.com>
2017-08-24 18:42:39 +0900

    BuilAll script: Adding Maya 2018

--- buildAll.bat
+++ buildAll.bat
@@ -6,4 +6,5 @@ scons -j8 no-cache=1 with-maya=2015
 scons -j8 no-cache=1 with-maya=2016
 scons -j8 no-cache=1 with-maya=2016.5
 scons -j8 no-cache=1 with-maya=2017
+scons -j8 no-cache=1 with-maya=2018
 
