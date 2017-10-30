1aebec12119c445e1ec3aa7f7c02e042eba6b8a6
miquelcampos <miquel.campos@gmail.com>
2017-10-30 16:26:49 +0900

    Shifter: Build command review and log popup window. import pm fix #73

--- scripts/mgear/maya/shifter/gui.py
+++ scripts/mgear/maya/shifter/gui.py
@@ -155,7 +155,7 @@ class Guide_UI(object):
         logWin = pm.window( title="mGear Build Log", iconName='mGear Log')
         pm.columnLayout( adjustableColumn=True )
         pm.cmdScrollFieldReporter(width=800, height=500, clr=True)
-        pm.button( label='Close', command=('pm.deleteUI(\"' + logWin + '\", window=True)') )
+        pm.button( label='Close', command=('import pymel.core as pm\npm.deleteUI(\"' + logWin + '\", window=True)') )
         pm.setParent( '..' )
         pm.showWindow( logWin )
         print mgear.logInfos()
