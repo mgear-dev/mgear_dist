f7c527642030aae7048292b5be3e0574995612c3
miquelcampos <miquel.campos@gmail.com>
2017-11-20 13:52:14 +0900

    Revert "PEP8 #100 : Shifter:Refactoring icon import"
    This reverts commit 0235e59a56f1712b034644b9f2e43074fefbfbaf.

--- scripts/mgear/maya/shifter/component/squash_01/guide.py
+++ scripts/mgear/maya/shifter/component/squash_01/guide.py
@@ -1,18 +1,53 @@
+# MGEAR is under the terms of the MIT License
+
+# Copyright (c) 2016 Jeremie Passerin, Miquel Campos
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# Author:     Jeremie Passerin      geerem@hotmail.com  www.jeremiepasserin.com
+# Author:     Miquel Campos         hello@miquel-campos.com  www.miquel-campos.com
+# Date:       2016 / 10 / 10
+
+##########################################################
+# GLOBAL
+##########################################################
 from functools import partial
 
-from mgear.maya.shifter.component import guide
-from mgear.maya import transform, pyqt
+
+# mgear
+from mgear.maya.shifter.component.guide import ComponentGuide
+import mgear.maya.transform as tra
 import settingsUI as sui
-from mgear.vendor.Qt import QtWidgets, QtCore
+
+
+#Pyside
 from mgear.maya.shifter.component.guide import componentMainSettings
+import mgear.maya.pyqt as gqt
 from maya.app.general.mayaMixin import MayaQWidgetDockableMixin
 from maya.app.general.mayaMixin import MayaQDockWidget
+QtGui, QtCore, QtWidgets, wrapInstance = gqt.qt_import()
 
 # guide info
 AUTHOR = "Miquel Campos"
 URL = "www.miquel-campos.com"
 EMAIL = "hello@miquel-campos.com"
-VERSION = [1, 0, 0]
+VERSION = [1,0,0]
 TYPE = "squash_01"
 NAME = "squash"
 DESCRIPTION = "Linear squash component"
@@ -20,9 +55,7 @@ DESCRIPTION = "Linear squash component"
 ##########################################################
 # CLASS
 ##########################################################
-
-
-class Guide(guide.ComponentGuide):
+class Guide(ComponentGuide):
 
     compType = TYPE
     compName = NAME
@@ -33,37 +66,41 @@ class Guide(guide.ComponentGuide):
     email = EMAIL
     version = VERSION
 
+
+    # =====================================================
+    ##
+    # @param self
     def postInit(self):
         self.save_transform = ["root", "tip"]
         self.save_blade = ["blade"]
 
+    # =====================================================
+    ## Add more object to the object definition list.
+    # @param self
     def addObjects(self):
 
         self.root = self.addRoot()
-        vTemp = transform.getOffsetPosition(self.root, [2, 0, 0])
+        vTemp = tra.getOffsetPosition( self.root, [2,0,0])
         self.loc = self.addLoc("tip", self.root, vTemp)
         self.blade = self.addBlade("blade", self.root, self.loc)
 
         centers = [self.root, self.loc]
         self.dispcrv = self.addDispCurve("crv", centers)
 
+
+
     # =====================================================
-    # Add more parameter to the parameter definition list.
+    ## Add more parameter to the parameter definition list.
     # @param self
     def addParameters(self):
 
-        self.pRefArray = self.addParam("ikrefarray", "string", "")
+        self.pRefArray  = self.addParam("ikrefarray", "string", "")
         self.pUseIndex = self.addParam("useIndex", "bool", False)
-        self.pParentJointIndex = self.addParam("parentJointIndex",
-                                               "long",
-                                               -1,
-                                               None,
-                                               None)
+        self.pParentJointIndex = self.addParam("parentJointIndex", "long", -1, None, None)
 
 ##########################################################
 # Setting Page
 ##########################################################
-
 
 class settingsTab(QtWidgets.QDialog, sui.Ui_Form):
 
@@ -74,13 +111,14 @@ class settingsTab(QtWidgets.QDialog, sui.Ui_Form):
 
 class componentSettings(MayaQWidgetDockableMixin, componentMainSettings):
 
-    def __init__(self, parent=None):
+    def __init__(self, parent = None):
         self.toolName = TYPE
         # Delete old instances of the componet settings window.
-        pyqt.deleteInstances(self, MayaQDockWidget)
+        gqt.deleteInstances(self, MayaQDockWidget)
 
-        super(self.__class__, self).__init__(parent=parent)
+        super(self.__class__, self).__init__(parent = parent)
         self.settingsTab = settingsTab()
+
 
         self.setup_componentSettingWindow()
         self.create_componentControls()
@@ -89,7 +127,7 @@ class componentSettings(MayaQWidgetDockableMixin, componentMainSettings):
         self.create_componentConnections()
 
     def setup_componentSettingWindow(self):
-        self.mayaMainWindow = pyqt.maya_main_window()
+        self.mayaMainWindow = gqt.maya_main_window()
 
         self.setObjectName(self.toolName)
         self.setWindowFlags(QtCore.Qt.Window)
@@ -100,13 +138,11 @@ class componentSettings(MayaQWidgetDockableMixin, componentMainSettings):
         return
 
     def populate_componentControls(self):
-        """Populate the controls values.
-
-        Populate the controls values from the custom attributes of the
-        component.
+        """
+        Populate the controls values from the custom attributes of the component.
 
         """
-        # populate tab
+        #populate tab
         self.tabs.insertTab(1, self.settingsTab, "Component Settings")
 
         refArrayItems = self.root.attr("ikrefarray").get().split(",")
@@ -123,14 +159,8 @@ class componentSettings(MayaQWidgetDockableMixin, componentMainSettings):
 
     def create_componentConnections(self):
 
-        self.settingsTab.refArrayAdd_pushButton.clicked.connect(
-            partial(self.addItem2listWidget,
-                    self.settingsTab.refArray_listWidget,
-                    "ikrefarray"))
-        self.settingsTab.refArrayRemove_pushButton.clicked.connect(
-            partial(self.removeSelectedFromListWidget,
-                    self.settingsTab.refArray_listWidget,
-                    "ikrefarray"))
+        self.settingsTab.refArrayAdd_pushButton.clicked.connect(partial(self.addItem2listWidget, self.settingsTab.refArray_listWidget, "ikrefarray"))
+        self.settingsTab.refArrayRemove_pushButton.clicked.connect(partial(self.removeSelectedFromListWidget, self.settingsTab.refArray_listWidget, "ikrefarray"))
         self.settingsTab.refArray_listWidget.installEventFilter(self)
 
     def eventFilter(self, sender, event):
@@ -142,4 +172,4 @@ class componentSettings(MayaQWidgetDockableMixin, componentMainSettings):
             return QtWidgets.QDialog.eventFilter(self, sender, event)
 
     def dockCloseEventTriggered(self):
-        pyqt.deleteInstances(self, MayaQDockWidget)
+        gqt.deleteInstances(self, MayaQDockWidget)
