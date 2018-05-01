e1337c2b10fe7e72f917dff94b5fb0bd51b77665
miquelcampos <miquel.campos@gmail.com>
2018-05-02 05:28:06 +0900

    Shifter: Partial build skip custom steps Closes #154

--- scripts/mgear/maya/shifter/__init__.py
+++ scripts/mgear/maya/shifter/__init__.py
@@ -118,7 +118,11 @@ class Rig(object):
                 mgear.sev_error)
             return
 
-        self.preCustomStep(selection)
+        # check if is partial build or full guide build
+        ismodel = False
+        if selection[0].hasAttr("ismodel"):
+            self.preCustomStep(selection)
+            ismodel = True
 
         if not self.stopBuild:
             mgear.log("\n" + "= GUIDE VALIDATION " + "=" * 46)
@@ -130,7 +134,8 @@ class Rig(object):
             # Build
             mgear.log("\n" + "= BUILDING RIG " + "=" * 46)
             self.build()
-            self.postCustomStep()
+            if ismodel:
+                self.postCustomStep()
 
             endTime = datetime.datetime.now()
             finalTime = endTime - startTime
