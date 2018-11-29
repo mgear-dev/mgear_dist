b111c2f8c97c9c415ff6f1dfb16947b271a0c887
Jerome Drese <jdrese@gmail.com>
2018-11-29 10:08:13 +0100

    add Maya 2019 build support on buildAll scripts

--- buildAll.bat
+++ buildAll.bat
@@ -1,10 +1,8 @@
 @echo off
 
-
-
 scons -j8 no-cache=1 with-maya=2015
 scons -j8 no-cache=1 with-maya=2016
 scons -j8 no-cache=1 with-maya=2016.5
 scons -j8 no-cache=1 with-maya=2017
 scons -j8 no-cache=1 with-maya=2018
-
+scons -j8 no-cache=1 with-maya=2019
