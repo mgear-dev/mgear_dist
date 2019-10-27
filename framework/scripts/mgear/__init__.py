c71fc16ff607b5dcd6f251b8bc96e2ef01f9a897
miquelcampos <miquel.campos@gmail.com>
2019-10-28 05:14:33 +0900

    update to version 3.2.1

--- framework/scripts/mgear/__init__.py
+++ framework/scripts/mgear/__init__.py
@@ -21,7 +21,7 @@ sev_verbose = 16
 sev_comment = 32
 
 # gear version
-VERSION = [3, 2, 0]
+VERSION = [3, 2, 1]
 
 self = sys.modules[__name__]
 self.menu_id = None
--- framework/scripts/mgear/version.py
+++ framework/scripts/mgear/version.py
@@ -2,7 +2,7 @@
 
 VERSION_MAJOR = 3
 VERSION_MINOR = 2
-VERSION_PATCH = 0
+VERSION_PATCH = 1
 
 version_info = (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
 version = '%i.%i.%i' % version_info
