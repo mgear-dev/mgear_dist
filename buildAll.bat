7c94038b3f20c266e12002ead6d08746cc05a039
miquelcampos <miquel.campos@gmail.com>
2021-02-26 05:30:07 +0900

    dropping support for Maya 2017 and older. Closes #60

--- buildAll.bat
+++ buildAll.bat
@@ -1,8 +1,5 @@
 @echo off
 
-scons -j8 no-cache=1 with-maya=2015
-scons -j8 no-cache=1 with-maya=2016
-scons -j8 no-cache=1 with-maya=2016.5
-scons -j8 no-cache=1 with-maya=2017
 scons -j8 no-cache=1 with-maya=2018
 scons -j8 no-cache=1 with-maya=2019
+scons -j8 no-cache=1 with-maya=2020
