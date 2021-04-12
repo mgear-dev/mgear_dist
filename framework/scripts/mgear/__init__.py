6737b261c1a972b7acbc94d64786193fdef43ef6
Miquel Campos <miquel.campos@gmail.com>
2021-04-13 06:25:29 +0900

    update version number 3.7.11

--- framework/scripts/mgear/__init__.py
+++ framework/scripts/mgear/__init__.py
@@ -21,7 +21,7 @@ sev_verbose = 16
 sev_comment = 32
 
 # gear version
-VERSION = [3, 7, 10]
+VERSION = [3, 7, 11]
 
 self = sys.modules[__name__]
 self.menu_id = None
--- framework/scripts/mgear/version.py
+++ framework/scripts/mgear/version.py
@@ -2,7 +2,7 @@
 
 VERSION_MAJOR = 3
 VERSION_MINOR = 7
-VERSION_PATCH = 10
+VERSION_PATCH = 11
 
 version_info = (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
 version = '%i.%i.%i' % version_info
